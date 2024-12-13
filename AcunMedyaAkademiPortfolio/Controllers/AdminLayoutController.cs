c7ac0607fbbcba5ced79f1d5deb1bb9281381763
FATIH\FATIH <fatihxserbest@gmail.com>
2024-12-13 13:57:04 +0300

    Add project files.

---
+++ AcunMedyaAkademiPortfolio/Controllers/AdminLayoutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        // GET: AdminLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult _HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _ScriptsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ AcunMedyaAkademiPortfolio/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string birlestir()
+        {
+            string cumle1 = "Merhaba";
+            string cumle2 = "Acunmedya Öğrencileri";
+            string cumle3=cumle1 + cumle2;
+            return cumle3;
+        }
+    }
+}
